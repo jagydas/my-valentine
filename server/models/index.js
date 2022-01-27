86a4d5d4ade32595d0d8a5b508a0a725d35a294a
Jagy <jagy.das12@gmail.com>
2022-01-27 10:37:31 -0500

    added seed

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
-const Product = require('./Product');
+const Gift = require('./Gift');
 const Category = require('./Category');
 const Order = require('./Order');
 
-module.exports = { User, Product, Category, Order };
+module.exports = { User, Gift, Category, Order };
