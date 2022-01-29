df847b11ab45ed9423ffe204fc737223bf4c0951
Mital <mitalg0717@gmail.com>
2022-01-29 18:27:45 -0500

    changes made in nav bar tab

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -34,16 +34,16 @@ function Nav() {
     } else {
       return (
         <ul className="flex-row">
-          <li className="mx-1">
+              <li className="mx-1">
             <Link to="/signup">
               Signup
             </Link>
-          </li>
-          <li className="mx-1">
+            </li>
+            <li className="mx-1">
             <Link to="/login">
               Login
             </Link>
-          </li>
+            </li>
         </ul>
       );
     }
